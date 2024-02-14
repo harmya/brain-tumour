7f03944e4b2e113c7a8f9db1f8539060a83f3c21
harmya <hvbhatt@purdue.edu>
2024-02-14 17:48:35 -0500

    first final version

--- src/binary-classify.py
+++ src/binary-classify.py
@@ -3,15 +3,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+import time
 
 print("------------Check if CUDA is available-------------")
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
 
+'''
+device = torch.device("mps" if torch.mps.is_available() else "cpu")
+print("Device:", device)
+'''
+
 print("------------Important Variables----------------")
-batch_size = 8
-learning_rate = 0.01
-epochs = 10
+batch_size = 32
+learning_rate = 0.005
+epochs = 20
 kernel_size = 5
 print("Batch Size:", batch_size)
 print("Learning Rate:", learning_rate)
@@ -23,9 +29,10 @@ print(".")
 print(".")
 print(".")
 training = torch.load('torch-dataset/training.pt')
-training = training[:100]
+# training = training[:256]
 validation = torch.load('torch-dataset/validation.pt')
 testing = torch.load('torch-dataset/testing.pt')
+# testing = testing[:256]
 
 d_train = torch.utils.data.DataLoader(training, batch_size=batch_size, shuffle=True)
 d_vali = torch.utils.data.DataLoader(validation, batch_size=batch_size, shuffle=True)
@@ -45,82 +52,98 @@ print("--------------------------------------------------")
 
 print("--------------Model Architecture------------------")
 model = nn.Sequential(
-    nn.Conv2d(1, 32, kernel_size=kernel_size, stride=1, padding=2),
+    nn.Conv2d(1, 32, kernel_size=kernel_size, stride=2, padding=1),
     nn.ReLU(),
-    nn.MaxPool2d(kernel_size=2, stride=1),
+    nn.MaxPool2d(kernel_size=3, stride=1),
 
-    nn.Conv2d(32, 64, kernel_size=kernel_size, stride=1, padding=2),
+    nn.Conv2d(32, 64, kernel_size=kernel_size, stride=2, padding=1),
     nn.ReLU(),
-    nn.MaxPool2d(kernel_size=2, stride=1),
+    nn.MaxPool2d(kernel_size=3, stride=1),
+
+    nn.Conv2d(64, 128, kernel_size=kernel_size, stride=2, padding=1),
+    nn.ReLU(),
+    nn.AvgPool2d(kernel_size=kernel_size, stride=1),
 
     nn.Flatten(),
-    nn.Linear(64 * 254 * 254, 256),
+    nn.Linear(128 * 25 * 25, 256),
     nn.ReLU(),
     nn.Linear(256, 1),
     nn.Sigmoid()
 )
 
-# model.to(device)
+# check the output size
+print("-------------------Checking input-output compatibility-------------------")
+print("Input shape: ", training[0][0].shape)
+x = training[0][0].unsqueeze(0)
+out = model(x)
+print("Output shape: ", out.shape)
+print("Output: ", out.flatten().item())
+
+model.to(device)
 print(model)
 print("-------------------------------------------------\n")
 
 
 print("--------------Loss Function and Optimizer---------")
-loss_function = nn.MSELoss()
+loss_function = nn.BCELoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 print("Loss Function:", loss_function)
 print("Optimizer:", optimizer)
 print("-------------------------------------------------\n")
 
 
-
-print("--------------Training Loop-----------------------")
+print("--------------Training Loop------------------------")
+# training loop
 for epoch in range(epochs):
     running_loss = 0.0
     model.train()
     for image, label in d_train:
-       # image, label = image.to(device), label.to(device)
-        
-        output = model(image)
-        loss = loss_function(output, label.float().unsqueeze(1))
-
+        image, label = image.to(device), label.to(device)
         optimizer.zero_grad()
+        output = model(image)
+        loss = loss_function(output.flatten(), label.float())
         loss.backward()
         optimizer.step()
-    
-    # check validation loss and accuracy
+        running_loss += loss.item()
+
     with torch.no_grad():
         model.eval()
+        val_loss = 0.0
         correct = 0
         total = 0
-        all_val_loss = []
         for image, label in d_vali:
- #           image, label = image.to(device), label.to(device)
+            image, label = image.to(device), label.to(device)
             output = model(image)
-            val_loss = loss_function(output, label.float().unsqueeze(1))
-            print(output)
-            print(label)
-            predicted = torch.round(output).squeeze(1).int()
-            print(predicted)
-            total += predicted.size(0)
-            print(predicted.size(0))
-            correct += (predicted == label).sum().item()
-            print((predicted == label).sum().item())
-        print(f"Epoch {epoch+1}, validation loss: {val_loss.item()}, validation accuracy: {100 * correct / total}%")
+            loss = loss_function(output.flatten(), label.float())
+            val_loss += loss.item()
+            output = (output > 0.5).flatten().int()
+            correct += (output == label).sum().item()
+            total += output.size(0)
+        
+        print(f"Epoch {epoch+1}, training loss: {running_loss / len(d_train)}, val_loss: {val_loss / len(d_vali)}, val_acc: {correct / total}")
+
+
 print("-------------------------------------------------\n")
 
 
 print("--------------Testing Loop------------------------")
 correct = 0
 total = 0
+running_loss = 0.0
+model.eval()
 with torch.no_grad():
     for image, label in d_test:
         image, label = image.to(device), label.to(device)
         output = model(image)
-        predicted = torch.round(output)
-        total += 1
-        correct += 1 if predicted == label else 0
-print(f"Test Accuracy: {100 * correct / total}%")
+
+        loss = loss_function(output.flatten(), label.float())
+        running_loss += loss.item()
+
+        output = (output > 0.5).flatten().int()
+        correct += (output == label).sum().item()
+        total += output.size(0)
+
+print(f"Test Loss: {running_loss / len(d_test)}, Test Accuracy: {correct / total}")
 print("-------------------------------------------------\n")
 print("-------------------------------------------------")
 
